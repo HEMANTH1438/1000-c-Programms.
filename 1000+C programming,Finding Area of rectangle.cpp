592059fde0e00166f90b03acf9513c626e2c3c2d
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-12-06 19:18:56 +0530

    1000+C programming,Finding Area of rectangle.c

---
+++ 1000+C programming,Finding Area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,w,Area;
+    printf("Enter the length of a rectangle");
+    scanf("%d",&l);
+    printf("Enter the width of rectangle");
+    scanf("%d",&w);
+    Area=l*w;
+    printf("The area of rectangle is:%d",Area);
+    return 0;
+}
