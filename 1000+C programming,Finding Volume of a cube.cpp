3ab29283132f43a42eed31fe3c7826279e43f77d
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-12-06 19:20:39 +0530

    1000+C programming,Finding Volume of a cube.c

---
+++ 1000+C programming,Finding Volume of a cube.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,volume;
+    printf("Enter the value of side of a cube");
+    scanf("%d",&a);
+    volume=a*a*a;
+    printf("The volume of cube is:%d",volume);
+    return 0;
+}
