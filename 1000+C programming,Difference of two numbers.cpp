c9cd0c2e4ac874ea4edfec24b4b6d092a2b9f6a1
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-12-06 19:14:24 +0530

    1000+C programming,Difference of two numbers.c

---
+++ 1000+C programming,Difference of two numbers.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,diff;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    diff=a-b;
+    printf("The difference of a and b is:%d",diff);
+}
