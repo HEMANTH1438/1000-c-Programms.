4b7dc939f8ca42ed11c9453ab2e89e2c882531d2
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-12-06 19:16:09 +0530

    1000+C programming,Finding Area of square.c

---
+++ 1000+C programming,Finding Area of square.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a,Area;
+    printf("Enter the value of side of a square");
+    scanf("%f",&a);
+    Area=a*a;
+    printf("The area of square is:%.1f",Area);
+    return 0;
+}
