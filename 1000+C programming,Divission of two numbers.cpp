62e814bff05785aadb40fc1adffd1831c8bad70c
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-12-06 19:14:59 +0530

    1000+C programming,Divission of two numbers.c

---
+++ 1000+C programming,Divission of two numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    float div;
+    printf("Enter the value of a");
+    scanf("%d",&a);
+    printf("Enter the value of b");
+    scanf("%d",&b);
+    div=a/b;
+    printf("The divission of a and b is:%.1f",div);
+}
