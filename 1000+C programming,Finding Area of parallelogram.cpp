ba7a3230541031197eff9e0c255772dd2a00f73f
HEMANTH1438 <93912688+HEMANTH1438@users.noreply.github.com>
2021-12-06 19:19:35 +0530

    1000+C programming,Finding Area of parallelogram.c

---
+++ 1000+C programming,Finding Area of parallelogram.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int b,h,Area;
+    printf("Enter the base of a parallelogram");
+    scanf("%d",&b);
+    printf("Enter the height of parallelogram");
+    scanf("%d",&h);
+    Area=b*h;
+    printf("The area of parallelogram is:%d",Area);
+    return 0;
+}
